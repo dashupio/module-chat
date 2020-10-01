9c7d7717ba0c53c4a78436e3afa22a891e2b956d
Dashup <git@dashup.io>
2020-10-01 21:39:23 +1000

    more listeners

--- pages/channel.ts
+++ pages/channel.ts
@@ -6,6 +6,19 @@ import { Struct } from '@dashup/module';
  * build address helper
  */
 export default class ChannelPage extends Struct {
+  /**
+   * construct discord connector
+   *
+   * @param args 
+   */
+  constructor(...args) {
+    // run super
+    super(...args);
+    
+    // bind methods
+    this.sendAction = this.sendAction.bind(this);
+    this.listenAction = this.listenAction.bind(this);
+  }
 
   /**
    * returns page type
@@ -42,6 +55,17 @@ export default class ChannelPage extends Struct {
   /**
    * returns object of views
    */
+  get actions() {
+    // return object of views
+    return {
+      send   : this.sendAction,
+      listen : this.listenAction,
+    };
+  }
+
+  /**
+   * returns object of views
+   */
   get views() {
     // return object of views
     return {
@@ -67,5 +91,42 @@ export default class ChannelPage extends Struct {
   get description() {
     // return description string
     return 'Internal chat channel';
+  }
+
+  /**
+   * send action
+   *
+   * @param param0 
+   * @param message 
+   * @param embeds 
+   */
+  async sendAction({ session, user }, { subject, message, embeds }) {
+    // create message
+    const actualMessage = await this.dashup.connection.rpc('update.message', user, {
+      embeds,
+      subject,
+      message,
+    });
+
+    // emit to room
+    this.dashup.connection.rpc('socket.room', subject, `${subject}.messages`, [actualMessage]);
+
+    // return message
+    return actualMessage;
+  }
+
+  /**
+   * listen action
+   *
+   * @param data 
+   * @param subject 
+   */
+  async listenAction({ session }, subject) {
+    // listen
+    const messages = await this.dashup.connection.rpc('query.message', subject);
+
+    // listen
+    this.dashup.connection.rpc('socket.emit', session, `${subject}.messages`, messages);
+    this.dashup.connection.rpc('subscribe.message', session, subject);
   }
 }
